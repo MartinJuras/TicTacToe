d8e7c01d89f280764dddd95a6f22210e24a4f86e
Martin Juras <Th3gr3y8unny@gmail.com>
2023-07-31 19:49:24 +0300

    7/31/2023

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+import View from "./view.js";
+import Store from "./store.js";
+
 const App = {
   // All of our selected HTML elements
   $: {
@@ -153,4 +156,61 @@ const App = {
   },
 };
 
-window.addEventListener("load", App.init);
+const players = [
+  {
+    id: 1,
+    name: "Player 1",
+    iconClass: "fa-x",
+    colorClass: "turquoise",
+  },
+  {
+    id: 2,
+    name: "Player 2",
+    iconClass: "fa-o",
+    colorClass: "yellow",
+  },
+];
+
+function init() {
+  const view = new View();
+  const store = new Store(players);
+
+  view.bindGameResetEvent((event) => {
+    console.log("Reset Event");
+    console.log(event);
+  });
+
+  view.bindNewRoundEvent((event) => {
+    console.log("New Round Event");
+    console.log(event);
+  });
+
+  view.bindPlayerMoveEvent((square) => {
+    const existingMove = store.game.moves.find(
+      (move) => move.squareId === +square.id
+    );
+    if (existingMove) {
+      return;
+    }
+
+    // Place an icon of the current player in  a square
+    view.handlePlayerMove(square, store.game.currentPlayer);
+
+    // Advance to the next state by pushing a move to the moves array
+    store.playerMove(+square.id);
+
+    if (store.game.status.isComplete) {
+      view.openModal(
+        store.game.status.winner
+          ? `${store.game.status.winner.name} wins!`
+          : "Ties!"
+      );
+      return;
+    }
+
+    // Set the next player's turn indicator
+    view.setTurnIndicator(store.game.currentPlayer);
+  });
+}
+
+window.addEventListener("load", init);
